373f456136d8b01a9c9f4e3a687e544c10701260
maarten.vdbul <maarten.vdbul@gmail.com>
2023-10-11 21:46:54 +0200

    ✨ Add eval module

---
+++ ml_factory/eval/evaluator.py
@@ -0,0 +1,12 @@
+from sklearn.metrics import classification_report, roc_auc_score
+
+
+class Evaluator:
+
+    @staticmethod
+    def get_classification_report(y_true, y_preds):
+        return classification_report(y_true, y_preds)
+
+    @staticmethod
+    def get_roc_auc_score(y_true, y_preds) -> float:
+        return roc_auc_score(y_true, y_preds)
